6717a8344bfa14a571003116487c532fd3e154c4
Sammy Yi <sammyhyi@gmail.com>
2023-06-14 03:00:27 -0400

    set up schemas folder

--- server/server.js
+++ server/server.js
@@ -1,13 +1,33 @@
 const express = require('express');
+// apollo-server-express
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas')
+//mongoose connector
 const db = require('./config/connection');
-const models = require('./models');
+
+//PORT
+const PORT = process.env.PORT || 3001;
+
+//creates new ApolloServer
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers
+});
 
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+app.use(express.urlencoded({extended: true}));
+app.use(express.json);
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
+// start ApolloServer, then connect to express, then connect to mongoose and lastly start the app
+const startApolloServer = async () => {
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+    })
   })
-})
+}
+
+startApolloServer();
