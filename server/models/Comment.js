920a2472ddad454c5338be4f98118d3335df4c65
jamiec1077 <jamie.charlemagne1992@gmail.com>
2023-06-15 10:43:27 -0400

    update

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const { Schema, Types } = mongoose;
 
-const commentSchema = new Schema ({
+const commentSchema = new Schema({
   commentId: {
     type: Schema.Types.ObjectId,
     default: () => new Types.ObjectId(),
@@ -21,6 +21,34 @@ const commentSchema = new Schema ({
     default: Date.now,
     get: createdDate => new Date(createdDate).toLocaleDateString()
   },
-})
+});
 
-module.exports = commentSchema;
+const Comment = mongoose.model('Comment', commentSchema);
+
+// Establish a database connection
+mongoose.connect('((//mongodb://localhost/mydatabase// what should i put as a name here ))', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to the database');
+  })
+  .catch((error) => {
+    console.error('Error connecting to the database', error);
+  });
+
+async function createComment(commentData) {
+  try {
+    const comment = new Comment(commentData);
+    const savedComment = await comment.save();
+    return savedComment;
+  } catch (error) {
+    console.error('Error creating comment', error);
+    throw error;
+  }
+}
+
+module.exports = {
+  Comment,
+  createComment,
+};
