e71da089e6fadd5e15918ee6027a99624b6bcf64
jamiec1077 <jamie.charlemagne1992@gmail.com>
2023-06-18 10:27:27 -0400

    update

--- client/src/App.js
+++ client/src/App.js
@@ -2,25 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import CommentBox from './components/Starter/CommentBox';
-// import Clips from './components/Clips';
 
-
-const client = new ApolloClient ({
+const client = new ApolloClient({
   uri: 'http://localhost:3001/graphql',
-  cache: new InMemoryCache()
-})
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <ApolloProvider client={client}>
-    <div className="text-3xl font-bold underline">
-      
-      <CommentBox />
-      
-              {/* <Clips /> */}
-    </div>
+      <div className="text-3xl font-bold underline">
+        <CommentBox />
+        {/* <Clips /> */}
+      </div>
     </ApolloProvider>
-
   );
 }
 
