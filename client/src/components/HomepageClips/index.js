fe430b911bd402df046bf56c2d5b92cc3492ab1c
Sammy Yi <sammyhyi@gmail.com>
2023-06-18 03:04:20 -0400

    got embedded video players to work and styled cards

--- client/src/components/HomepageClips/index.js
+++ client/src/components/HomepageClips/index.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Box } from '@mui/material';
 
 const HomepageClips = () => {
   const [homeClipsData, setHomeClipsData] = useState([]);
@@ -18,10 +19,10 @@ const HomepageClips = () => {
             headers: headersList,
           }
         );
-
+          console.log(response)
         if (response.ok) {
-          const data = await response.json();
-          setHomeClipsData(data.data);
+          const { data } = await response.json();
+          setHomeClipsData(data);
         } else {
           console.error("Request failed with status:", response.status);
         }
@@ -39,48 +40,49 @@ const HomepageClips = () => {
   };
 
   return (
-    <section className="container mx-auto mt-10 grid grid-cols-5 gap-4">
-      {homeClipsData.map((clip) => (
-        <div
-          key={clip.id}
-          className="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-2 flex flex-col"
+<section className="container mx-auto mt-10 grid grid-cols-4 gap-4">
+  {homeClipsData.map((clip) => (
+    <div
+      key={clip.id}
+      className="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-2 flex flex-col"
+
+    >
+      <div className="aspect-w-16 aspect-h-9">
+        <Box
+          mb={3}
+          sx={{
+            position: 'relative',
+            paddingBottom: '60%',
+          }}
         >
-          <a href={clip.url} target="_blank" rel="noopener noreferrer">
-            <img className="rounded-lg" src={clip.thumbnail_url} alt="" />
-          </a>
-          <div className="p-3">
-            <a href={clip.url} target="_blank" rel="noopener noreferrer">
-              <h5 className="mb-1 text-lg font-bold tracking-tight text-gray-900 dark:text-white">
-                {clip.broadcaster_name}
-              </h5>
-            </a>
-            <ul className="mb-2 text-base font-normal text-gray-700 dark:text-white">
-              <li className="overflow-hidden" style={{ maxHeight: "3rem" }}>
-                "{clip.title}"
-              </li>
-              <li>{clip.view_count} views</li>
-              <li>{formatClipDate(clip.created_at)}</li>
-            </ul>
-            <a
-              href={clip.url}
-              className="inline-flex items-center text-blue-600 hover:underline text-base"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Link to twitch clip
-              <svg
-                className="w-4 h-4 ml-1"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                {/* SVG paths */}
-              </svg>
-            </a>
-          </div>
-        </div>
-      ))}
-    </section>
+          <iframe
+            src={`https://clips.twitch.tv/embed?clip=${clip.id}&parent=localhost`}
+            title="Twitch Clip"
+            allowFullScreen
+            style={{
+              position: 'absolute',
+              top: '0',
+              left: '0',
+              width: '100%',
+              height: '100%',
+            }}
+          />
+        </Box>
+      </div>
+      <div className="p-3" style={{ marginTop: '-25px', marginBottom: '-15px' }}>
+        <a href={clip.url} target="_blank" rel="noopener noreferrer">
+          <h5 className="mb-1 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {clip.broadcaster_name}
+          </h5>
+        </a>
+        <ul className="mb-2 text-base font-normal text-gray-700 dark:text-white">
+          <li>clipped by: {clip.creator_name}</li>
+          <li>{formatClipDate(clip.created_at)}</li>
+        </ul>
+      </div>
+    </div>
+  ))}
+</section>
   );
 };
 
