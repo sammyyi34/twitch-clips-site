161c16deca0258d10afc77c214a7476a0c05c64a
jamiec1077 <jamie.charlemagne1992@gmail.com>
2023-06-18 17:09:58 -0400

    update code now i need to push to main

--- client/src/components/Starter/CommentBox.js
+++ client/src/components/Starter/CommentBox.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import moment from 'moment';
 
-
 function CommentBox() {
   const [comments, setComments] = useState([]);
   const [newComment, setNewComment] = useState('');
@@ -49,15 +48,15 @@ function CommentBox() {
 
   return (
     <div className="w-1/2 mx-auto">
-      <h2 className="text-center text-2xl mb-4 text-white">Place your comments here</h2>
+      <h2 className="text-center text-2xl mb-4 text-black">Place your comments here</h2>
       <form onSubmit={handleFormSubmit}>
-        <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+        <label htmlFor="message" className="block mb-2 text-sm font-medium text-black">
           Your message
         </label>
         <textarea
           id="message"
           rows="4"
-          className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="block p-2.5 w-full text-sm text-black bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
           placeholder="Write your thoughts here..."
           value={newComment}
           onChange={handleInputChange}
@@ -68,10 +67,10 @@ function CommentBox() {
         </button>
       </form>
       <div className="mt-4">
-        <h3 className="text-center text-xl mb-2 text-white">Comments</h3>
+        <h3 className="text-center text-xl mb-2 text-black">Comments</h3>
         {comments.map((comment, index) => (
           <div key={index} className="mb-2 border-b border-gray-300">
-            <p className="text-white">{comment}</p>
+            <p className="text-black">{comment}</p>
             <p className="text-sm text-gray-500 mt-1">{moment().format('MMMM Do YYYY, h:mm:ss a')}</p>
             <div className="flex justify-end">
               <button onClick={() => handleEditComment(index)} className="text-blue-500 text-sm font-medium mr-2">
@@ -89,11 +88,10 @@ function CommentBox() {
       <button
         data-modal-target="authentication-modal"
         data-modal-toggle="authentication-modal"
-        className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        type="button"
-        onClick={toggleModal}
+        className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+      
       >
-        login
+       your comments show here 
       </button>
 
       {/* Main modal */}
@@ -106,10 +104,10 @@ function CommentBox() {
         >
           <div className="relative w-full max-w-md max-h-full">
             {/* Modal content */}
-            <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <div className="relative bg-white rounded-lg shadow">
               <button
                 type="button"
-                className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
+                className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center"
                 data-modal-hide="authentication-modal"
                 onClick={toggleModal}
               >
@@ -129,12 +127,12 @@ function CommentBox() {
                 <span className="sr-only">Close modal</span>
               </button>
               <div className="px-6 py-6 lg:px-8">
-                <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h3>
+                <h3 className="mb-4 text-xl font-medium text-black">Sign in to our platform</h3>
                 <form className="space-y-6" action="#">
                   <div>
                     <label
                       htmlFor="email"
-                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      className="block mb-2 text-sm font-medium text-black"
                     >
                       Your email
                     </label>
@@ -142,7 +140,7 @@ function CommentBox() {
                       type="email"
                       name="email"
                       id="email"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                      className="bg-white border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                       placeholder="name@company.com"
                       required
                     />
@@ -150,7 +148,7 @@ function CommentBox() {
                   <div>
                     <label
                       htmlFor="password"
-                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      className="block mb-2 text-sm font-medium text-black"
                     >
                       Your password
                     </label>
@@ -159,7 +157,7 @@ function CommentBox() {
                       name="password"
                       id="password"
                       placeholder="••••••••"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                      className="bg-white border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                       required
                     />
                   </div>
@@ -170,35 +168,35 @@ function CommentBox() {
                           id="remember"
                           type="checkbox"
                           value=""
-                          className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+                          className="w-4 h-4 border border-gray-300 rounded bg-white focus:ring-3 focus:ring-blue-300"
                           required
                         />
                       </div>
                       <label
                         htmlFor="remember"
-                        className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                        className="ml-2 text-sm font-medium text-black"
                       >
                         Remember me
                       </label>
                     </div>
                     <a
                       href="#"
-                      className="text-sm text-blue-700 hover:underline dark:text-blue-500"
+                      className="text-sm text-blue-700 hover:underline"
                     >
                       Lost Password?
                     </a>
                   </div>
                   <button
                     type="submit"
-                    className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                   >
                     Login to your account
                   </button>
-                  <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
+                  <div className="text-sm font-medium text-gray-500">
                     Not registered?{' '}
                     <a
                       href="#"
-                      className="text-blue-700 hover:underline dark:text-blue-500"
+                      className="text-blue-700 hover:underline"
                     >
                       Create account
                     </a>
