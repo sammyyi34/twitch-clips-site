f6db07a644a7772dce61fde45aaa04f9298e16b8
Paulwjang3 <128998742+Paulwjang3@users.noreply.github.com>
2023-06-19 17:29:24 -0400

    components file?

--- client/src/Components/LoginModal/login.js
+++ client/src/Components/LoginModal/login.js
@@ -41,7 +41,7 @@ const LoginModal = (props) => {
   return (
     <>
       <button
-        className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
         type="button"
         onClick={() => setShowModal(true)}
       >
@@ -111,7 +111,7 @@ const LoginModal = (props) => {
                       </button>
                       <button
                         type="submit"
-                        className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
                         Login
                       </button>
                     </div>
--- client/src/Components/Navbar/navi.js
+++ client/src/Components/Navbar/navi.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Auth from "../../utils/auth";
-import LoginModal from '../LoginModal';
+import LoginModal from '../LoginModal/login';
 import SignupModal from '../SignupModal';
 
 function Navbar() {
@@ -28,14 +28,14 @@ function Navbar() {
                 <nav className="bg-white border-gray-200 dark:bg-gray-900">
                     <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                         <div className="flex md:order-2">
-                                <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                                <ul className="flex items-center flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                                         <li className='text-white'>
                                             Twitch Clips
                                         </li>
-                                        <li className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                                        <li>
                                         <LoginModal />
                                         </li>
-                                        <li className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                                        <li>
                                         <SignupModal />
                                         </li>
                                 </ul>
